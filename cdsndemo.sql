b9c63ef21f1134f4091241ae17202af692f2ceb7
Hgj <126059558+hyjiang123@users.noreply.github.com>
2025-04-26 22:23:09 +0800

    上传案例和建表语句

---
+++ cdsndemo.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `csdndemo` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `hits` int(15) DEFAULT NULL,
+  `comment` int(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=516 DEFAULT CHARSET=utf8mb4;
