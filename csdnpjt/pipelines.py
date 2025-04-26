20d39a9c1bc153371d17f829f0fcf1be57b85d54
Hgj <126059558+hyjiang123@users.noreply.github.com>
2025-04-26 21:20:48 +0800

    初始化项目

---
+++ csdnpjt/pipelines.py
@@ -0,0 +1,32 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+import logging
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from numpy import histogram
+
+
+class CsdnpjtPipeline:
+    def __init__(self):
+        self.conn = pymysql.connect(host="106.52.42.140",user="root",password="hgj1097229155",db="blog",port=3306)
+
+    def process_item(self, item, spider):
+        for j in range(0, len(item["name"])):
+            name = item["name"][j]
+            url = item["url"][j]
+            hits = item["hits"][j]
+            hits = str(hits)
+            comment = item["comment"][j]
+            comment = str(comment)
+            sql = "insert into csdndemo(name, url, hits, comment) VALUES('"+name+"','"+url+"','"+hits+"','"+comment+"')"
+
+            cs = self.conn.cursor()
+            cs.execute(sql)
+            self.conn.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.conn.close()
